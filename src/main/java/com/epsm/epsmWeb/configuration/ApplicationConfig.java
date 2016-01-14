ec475b79c3dd06878d385adfc2eb984e097d0a92
valeriy <epsm@i.ua>
2016-01-14 17:59:23 +0200

    @Bean TimeService added, loging added.

--- src/main/java/com/epsm/epsmWeb/configuration/ApplicationConfig.java
+++ src/main/java/com/epsm/epsmWeb/configuration/ApplicationConfig.java
@@ -1,21 +1,34 @@
 package com.epsm.epsmWeb.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 
+import com.epsm.epsmCore.model.generalModel.TimeService;
+
 @Configuration
 @ComponentScan("com.epsm.epsmWeb")
 public class ApplicationConfig{
+	private Logger logger = LoggerFactory.getLogger(ApplicationConfig.class);
 	
 	@Bean
-    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+    public PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
 		PropertySourcesPlaceholderConfigurer configurer 
 				= new PropertySourcesPlaceholderConfigurer();
 		configurer.setLocation(new ClassPathResource("application.properties"));
         
+		logger.debug("PropertySourcesPlaceholderConfigurer @Bean created.");
+		
         return configurer;
     }
+	
+	@Bean
+	public TimeService createTimeService(){
+		logger.debug("Timeservice @Bean created.");
+		return new TimeService();
+	}
 }
